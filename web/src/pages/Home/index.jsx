33e02e691560f720af12fc5e9ed87ce2317f9d52
Gabriel Luciano <gomeslucianogabriel@gmail.com>
2020-07-28 12:19:42 -0300

    Save point commit

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -14,25 +14,30 @@ function Home() {
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription="O que é o front end"
       />
+
       <Carousel
         ignoreFirstVideo
         category={dadosIniciais.categorias[0]}
       />
+
       <Carousel
-        ignoreFirstVideo
         category={dadosIniciais.categorias[1]}
       />
+
       <Carousel
-        ignoreFirstVideo
         category={dadosIniciais.categorias[2]}
-      />
+      />      
+
       <Carousel
-        ignoreFirstVideo
         category={dadosIniciais.categorias[3]}
-      />
+      />      
+
       <Carousel
-        ignoreFirstVideo
         category={dadosIniciais.categorias[4]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[5]}
       />
     </div>
   );
---
+++ web/src/pages/Register/CategoryRegister/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import PageDefault from '../../../components/PageDefault';
+
+
+const CategoryRegister = () => {
+  return (
+    <PageDefault>
+      <h1> Cadastro de categorias </h1>
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
+}
+
+export default CategoryRegister;
