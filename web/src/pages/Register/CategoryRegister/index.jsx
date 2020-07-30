217f532a461d4d17ca730858b50a9bf7c41b4084
Gabriel Luciano <gomeslucianogabriel@gmail.com>
2020-07-30 09:22:16 -0300

    Alterações básicas para o cadastro de categorias

--- web/src/pages/Register/CategoryRegister/index.jsx
+++ web/src/pages/Register/CategoryRegister/index.jsx
@@ -1,26 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
 
 
-const CategoryRegister = () => {
+const CategoryRegister = () => {  
+  const initialValues = { 
+    name: '',
+    description: '',
+    color: '#141414'
+  }
+  
+  const [ categoryList, setCategoryList ] = useState([]);
+  const [ categoryValues, setCategoryValues ] = useState(initialValues);
+
+  function handleValueChange (categoryInfos) {
+    const { value } = categoryInfos.target;
+    const name = categoryInfos.target.getAttribute('name');
+    setCategoryValues({
+      ...categoryValues,
+      [name]: value 
+    })
+  }
+
   return (
     <PageDefault>
-      <h1> Cadastro de categorias </h1>
-      <form>
+      <h1> Cadastro de Categoria: {categoryValues.name} </h1>
+      <form
+        onSubmit={ function handleSubmit(eventInfos) {
+          eventInfos.preventDefault();
+          setCategoryList([...categoryList, categoryValues]);
+          setCategoryValues(initialValues)
+        }}
+      >
 
-        <label>
-          Nome da Categoria:
-          <input
-            type="text"
-          />
-        </label>
+        <FormField
+          label='Nome da Categoria:'
+          name='name'
+          type='text'
+          value={categoryValues.name}
+          onChange={handleValueChange}
+        />
+
+        <FormField 
+          label='Descrição:'
+          name='description'
+          type='text'
+          isTextArea
+          value={categoryValues.description}
+          onChange={handleValueChange}        
+        />
+
+        <FormField
+          label='Cor:'
+          name='color'
+          type='color'
+          value={categoryValues.color}
+          onChange={handleValueChange}
+        />
 
         <button>
           Cadastrar
         </button>
       </form>
+
+      <ul>
+        {categoryList.map((category, index) => (
+          <li key={index}>
+            {category.name}
+          </li>
+        ))}
+      </ul>
 
 
       <Link to="/">
