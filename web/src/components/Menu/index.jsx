a784ad14fd7d50f702e782f3d83342d725c10364
Gabriel Luciano <gomeslucianogabriel@gmail.com>
2020-07-30 14:12:51 -0300

    Alteração do Logo

--- web/src/components/Menu/index.jsx
+++ web/src/components/Menu/index.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './styles.css';
+import { LogoImage, MenuWrapper } from './styles';
 import Logo from '../../assets/img/logo.png';
 import Button from '../Button';
 
 const Menu = () => {
   return (
-    <nav className="Menu">
+    <MenuWrapper className="Menu">
       <Link to="/">
-        <img className="Logo" src={Logo} alt='GFlix logo'/>
+        <LogoImage src={Logo} alt='GameFlix logo'/>
       </Link>
 
       <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo vídeo
       </Button>
-    </nav>
+    </MenuWrapper>
   );
 }
 
