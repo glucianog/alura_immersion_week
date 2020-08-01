2e1a57b0e8ebcb0da0bfc4d8e38f33fcd57b7857
Gabriel Luciano <gomeslucianogabriel@gmail.com>
2020-08-01 09:36:36 -0300

    alerts

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+import AlertMUITemplate from 'react-alert-template-mui';
+
 import './index.css';
 import App from './App';
 
+// optional configuration
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 3000,
+  offset: '30px',
+  // you can also just use 'scale'
+  transition: transitions.SCALE,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AlertProvider template={AlertMUITemplate} {...options}>
+      <App />
+    </AlertProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { useAlert } from 'react-alert';
 
-import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import PageDefault from '../../components/PageDefault';
+import categoriesRepository from '../../respositories/categories';
 
 const HomeWrapper = styled.div`
   background: var(--grayDark);
@@ -16,39 +17,46 @@ const HomeWrapper = styled.div`
 `;
 
 function Home() {
+  const [initialData, setInitialData] = useState([]);
+  const alert = useAlert();
+
+  useEffect(() => {
+    categoriesRepository.getAllWithVideos()
+      .then((categoriesWithVideos) => {
+        setInitialData(categoriesWithVideos);
+      })
+      .catch((err) => {
+        alert.show('An error occurred during the data retrieval. Try again later');
+      });
+  }, [alert]);
+
   return (
     <HomeWrapper>
-      <PageDefault>
-        <BannerMain
-          videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-          url={dadosIniciais.categorias[0].videos[0].url}
-          videoDescription="O que é o front end"
-        />
-
-        <Carousel
-          ignoreFirstVideo
-          category={dadosIniciais.categorias[0]}
-        />
-
-        <Carousel
-          category={dadosIniciais.categorias[1]}
-        />
-
-        <Carousel
-          category={dadosIniciais.categorias[2]}
-        />
-
-        <Carousel
-          category={dadosIniciais.categorias[3]}
-        />
-
-        <Carousel
-          category={dadosIniciais.categorias[4]}
-        />
-
-        <Carousel
-          category={dadosIniciais.categorias[5]}
-        />
+      <PageDefault paddingAll={0}>
+        {initialData.length === 0 && (<div> Loading ...</div>)}
+        {initialData.map((category, index) => {
+          if (index === 0) {
+            return (
+              <div key={category.id}>
+                <BannerMain
+                  videoTitle={initialData[0].videos[0].title}
+                  url={initialData[0].videos[0].url}
+                  videoDescription="O que front-end"
+                />
+                <Carousel
+                  ignoreFirstVideo
+                  category={initialData[0]}
+                />
+              </div>
+            );
+          }
+          return (
+            <Carousel
+              key={category.id}
+              category={category}
+            />
+          );
+        })}
       </PageDefault>
     </HomeWrapper>
   );
